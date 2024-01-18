f96c24f64869b15c524ee3b69687c5275758d692
EvanGudmestad <eagudmestad@ranken.edu>
2024-01-18 11:46:40 -0600

    Ch2 Demo Continued

---
+++ MySite/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using MySite.Models;
+using System.Diagnostics;
+
+namespace MySite.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+      
+
+    }
+}
